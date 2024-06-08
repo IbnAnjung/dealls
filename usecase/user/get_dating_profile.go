ab97c5158baea3c485f3e8eec00db347dc18910d
im.anggasaputra <im.anggasaputra@gmail.com>
2024-06-09 00:59:58 +0700

    remove limit validation on get profile

--- usecase/user/get_dating_profile.go
+++ usecase/user/get_dating_profile.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/IbnAnjung/dealls/entity/enuser"
-	coreerror "github.com/IbnAnjung/dealls/pkg/error"
 )
 
 func (uc *userUsecase) GetDatingUserProfile(ctx context.Context, userID string) (recUser enuser.User, err error) {
@@ -17,12 +16,6 @@ func (uc *userUsecase) GetDatingUserProfile(ctx context.Context, userID string) 
 
 	user, err := uc.userRepository.FindById(ctx, userID)
 	if err != nil {
-		return
-	}
-
-	// return err if > 10 and isPremium false
-	if len(swippedUserIDs) > 10 && !user.IsPremium {
-		err = coreerror.NewCoreError(coreerror.CoreErrorTypeUnprocessable, "swipe exceeds daily limit")
 		return
 	}
 
